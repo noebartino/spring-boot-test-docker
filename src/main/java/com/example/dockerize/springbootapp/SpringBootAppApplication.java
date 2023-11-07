53028862c98b413f627c675b1474daf0273db110
Noe Gutierrez <noeadahigr@gmail.com>
2023-11-06 19:47:10 -0600

    ajuste context
    ajuste

--- src/main/java/com/example/dockerize/springbootapp/SpringBootAppApplication.java
+++ src/main/java/com/example/dockerize/springbootapp/SpringBootAppApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping("/api/hello")
+@RequestMapping("/example/hello/spring")
 public class SpringBootAppApplication {
 
 	public static void main(String[] args) {
